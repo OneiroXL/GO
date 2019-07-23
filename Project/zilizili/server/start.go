a0a2889cc1faf526c17c094c4e2387de6af740e9
OneiroXL <871018736@qq.com>
2019-07-23 18:01:11 +0800

    增加cors

--- Project/zilizili/server/start.go
+++ Project/zilizili/server/start.go
@@ -11,6 +11,13 @@ func Start() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		AllowCredentials: true,
+	}))
 	model.Database(beego.AppConfig.String("sqlconn"))
 	beego.Run()
 }

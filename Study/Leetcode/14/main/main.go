45484c633e51b864289969c703da57c7c0c20f0c
Z-X-L <871018736@qq.com>
2020-07-02 18:06:04 +0800

    1.

--- Study/Leetcode/14/main/main.go
+++ Study/Leetcode/14/main/main.go
@@ -2,40 +2,43 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 
 func main()  {
-	var strArr [3]string = [3]string{"flower","flow","flight"}
+	var strArr [2]string = [2]string{"abdf","abdfasdasdasd"}
 
 	fmt.Println(LongestCommonPrefix(strArr[:]))
 }
 
 func LongestCommonPrefix(strs []string) string {
-	var temStr string = ""
-
-	for _, val := range strs {
-		temStr = temStr + val + ","
+	var res string = ""
+	if(len(strs) == 0){
+		return res
+	}
+	var lenght int = len(strs[0]) 
+	for i := 0; i < len(strs); i++ {
+		if(len(strs[i]) < lenght){
+			lenght = len(strs[i])
+		} 
+	}
+	var i int = 0
+	for(i < lenght){
+		var flag bool = true
+		var pre byte = strs[0][i]
+		for _, val := range strs {
+			if(val[i] != pre){
+				flag = false
+			}
+		}
+		if(flag){
+			res  = res + string(pre)
+		} else {
+			return res
+		}
+		i++
 	}
 
-	temStr = strings.Trim(temStr,",")
-	fmt.Println(temStr)
-	
-	var flagIndex int = 0
-	var length int = 0
-	var res int = 0
-	for i := 1; i < len(temStr); i++ {
-		if(temStr[i] == temStr[flagIndex]){
-			flagIndex++
-		}
-		if(length > res){
-			res = length	
-		}
-		length = i - flagIndex
-	}
-
-
-
-	return ""
+	return res
 }
+
